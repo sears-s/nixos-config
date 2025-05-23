80dee3c09b124c530d9ae983fbc6d03169f7b7ef
Sears Schulz <36250748+sears-s@users.noreply.github.com>
2025-05-23 15:04:11 -0700

    Fix CAC one shot service fail

--- home/cac.nix
+++ home/cac.nix
@@ -13,7 +13,7 @@ lib.mkIf specialArgs.cac {
   programs.firefox.policies.SecurityDevices.p11-kit-proxy = "${pkgs.p11-kit}/lib/p11-kit-proxy.so";
 
   # One shot service to add the module for Chromium browsers
-  systemd.user.services.chromium-smartcard = {
+  systemd.user.services.chromium-p11-kit-proxy = {
     Install.WantedBy = [ "default.target" ];
     Service = {
       Environment = [
@@ -22,21 +22,30 @@ lib.mkIf specialArgs.cac {
             with pkgs;
             [
               coreutils
+              gnugrep
               nssTools
             ]
           )
         }"
       ];
-      ExecStart = pkgs.writeShellScript "chromium-smartcard.sh" ''
+      ExecStart = pkgs.writeShellScript "chromium-p11-kit-proxy.sh" ''
         set -e
         nssdb=~/.pki/nssdb
-        mkdir -p $nssdb
-        modutil -force -dbdir sql:$nssdb -add p11-kit-proxy \
-          -libfile ${pkgs.p11-kit}/lib/p11-kit-proxy.so
-        echo "Smartcard module added to Chromium NSS database at $nssdb"
+        mod=p11-kit-proxy
+
+        # Check if the module already exists
+        if [ -d $nssdb ] && modutil -dbdir sql:$nssdb -list | grep -q $mod; then
+          echo "$mod module already exists in Chromium NSS database at $nssdb"
+        else
+          # Add the module
+          mkdir -p $nssdb
+          modutil -force -dbdir sql:$nssdb -add p11-kit-proxy \
+            -libfile ${pkgs.p11-kit}/lib/p11-kit-proxy.so
+          echo "$mod module added to Chromium NSS database at $nssdb"
+        fi
       '';
       Type = "oneshot";
     };
-    Unit.Description = "Adds smartcard module for Chromium browsers";
+    Unit.Description = "add p11-kit-proxy module to Chromium browsers for smartcard support";
   };
 }
